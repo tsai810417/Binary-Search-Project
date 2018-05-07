ee98f89624cf8e9a98f7ea29f8e557586f7999d3
tsai810417 <tsai_cheng@yahoo.com>
2018-05-06 22:16:30 -0700

    add a method to handle addChild to the node

--- lib/bst_node.rb
+++ lib/bst_node.rb
@@ -3,4 +3,22 @@ class BSTNode
   def initialize(value = nil)
     @value = value
   end
+
+  def addChild(value)
+    if self.value > value
+      if self.right
+        self.right.addChild(value)
+      else
+        self.right = BSTNode.new(value)
+      end
+    else
+      if self.left
+        self.left.addChild(value)
+      else
+        self.left = BSTNode.new(value)
+      end
+    end
+  end
+
+
 end
