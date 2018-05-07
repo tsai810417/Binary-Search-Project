5fb6b271fe3172c101e7f4290071cf53e1f43b67
tsai810417 <tsai_cheng@yahoo.com>
2018-05-06 22:24:03 -0700

    add the parent attr when creating a child

--- lib/bst_node.rb
+++ lib/bst_node.rb
@@ -1,21 +1,21 @@
 class BSTNode
   attr_accessor :left, :right, :parent, :value
-  def initialize(value = nil)
+  def initialize(value = nil, parent = nil)
     @value = value
   end
 
   def addChild(value)
-    if self.value > value
+    if value > self.value
       if self.right
         self.right.addChild(value)
       else
-        self.right = BSTNode.new(value)
+        self.right = BSTNode.new(value, self)
       end
     else
       if self.left
         self.left.addChild(value)
       else
-        self.left = BSTNode.new(value)
+        self.left = BSTNode.new(value, self)
       end
     end
   end
